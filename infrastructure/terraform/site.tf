491fe9717aafa802d820ed25cd841569a4c4afb0
George Adams IV (geowa4) <oss@geowa4.software>
2017-10-10 20:41:41 -0400

    Add Datadog webhook to GitHub repo.
    This adds all of the supported events. I'm fairly certain Datadog
    ignores most of them, but I'd rather send them all and let the recipient
    filter them down...for now.
    
    Closes #14.

--- infrastructure/terraform/site.tf
+++ infrastructure/terraform/site.tf
@@ -5,6 +5,11 @@ variable "site_domain" {
   default = "rocdev.org"
 }
 
+variable "datadog_api_key_for_github" {
+  type    = "string"
+  default = ""
+}
+
 resource "github_repository" "app_repo" {
   name               = "rocdev"
   description        = "The future of RocDev."
@@ -17,4 +22,43 @@ resource "github_repository" "app_repo" {
   allow_squash_merge = false
   allow_rebase_merge = true
   private            = false
+}
+
+resource "github_repository_webhook" "datadog_webhook" {
+  name       = "web"
+  repository = "${github_repository.app_repo.name}"
+  active     = true
+
+  events = [
+    "commit_comment",
+    "create",
+    "delete",
+    "deployment",
+    "deployment_status",
+    "gollum",
+    "issue_comment",
+    "issues",
+    "label",
+    "member",
+    "milestone",
+    "page_build",
+    "project",
+    "project_card",
+    "project_column",
+    "public",
+    "pull_request",
+    "pull_request_review",
+    "pull_request_review_comment",
+    "push",
+    "release",
+    "repository",
+    "status",
+    "team_add"
+  ]
+
+  configuration {
+    url          = "https://app.datadoghq.com/intake/webhook/github?api_key=${var.datadog_api_key_for_github}"
+    content_type = "form"
+    insecure_ssl = false
+  }
 }
