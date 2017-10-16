ebbbee8d03f29bcea716a085933e04cf5531ef4e
Dan Swain <dan.t.swain@gmail.com>
2017-10-16 15:55:40 -0400

    Test for unsubscribe failure

--- app/lib/rocdev/mailing_list/mailing_list.ex
+++ app/lib/rocdev/mailing_list/mailing_list.ex
@@ -1,5 +1,14 @@
 defmodule Rocdev.MailingList do
+  @moduledoc """
+  API for interacting with the email list
+
+  The email list is managed by SParkPost, so a lot of this is just wrapping
+  the SparkPost API
+  """
+
   alias SparkPost.SuppressionList
+
+  require Logger
 
   def unsubscribe_async(email) do
     _ = Task.start(fn -> unsubscribe(email) end)
@@ -7,6 +16,13 @@ defmodule Rocdev.MailingList do
   end
 
   def unsubscribe(email) do
-    SuppressionList.upsert_one(email, "non_transactional")
+    case SuppressionList.upsert_one(email, "non_transactional") do
+      {:ok, ""} -> :ok
+      {:error, error} ->
+        Logger.warn(fn ->
+          "Error unsubscribing '#{inspect email}': #{inspect error}"
+        end)
+        :error
+    end
   end
 end
