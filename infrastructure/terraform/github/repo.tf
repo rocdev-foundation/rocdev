3d4410553094244b4e78eae0efba1ea2f3f864db
George Adams IV (geowa4) <oss@geowa4.software>
2017-10-23 23:17:53 -0400

    Manage memberships and teams with Terraform.
    Closes #33.

---
+++ infrastructure/terraform/github/repo.tf
@@ -0,0 +1,64 @@
+variable "site_domain" {
+  type    = "string"
+  default = "rocdev.org"
+}
+
+variable "datadog_api_key_for_github" {
+  type    = "string"
+  default = ""
+}
+
+resource "github_repository" "app_repo" {
+  name               = "rocdev"
+  description        = "The future of RocDev."
+  homepage_url       = "https://${var.site_domain}"
+  license_template   = "MIT"
+  has_issues         = true
+  has_wiki           = false
+  has_downloads      = false
+  allow_merge_commit = false
+  allow_squash_merge = false
+  allow_rebase_merge = true
+  private            = false
+}
+
+resource "github_repository_webhook" "datadog_webhook" {
+  name       = "web"
+  repository = "${github_repository.app_repo.name}"
+  active     = true
+
+  events = [
+    "commit_comment",
+    "create",
+    "delete",
+    "deployment",
+    "deployment_status",
+    "fork",
+    "gollum",
+    "issue_comment",
+    "issues",
+    "label",
+    "member",
+    "milestone",
+    "page_build",
+    "project",
+    "project_card",
+    "project_column",
+    "public",
+    "pull_request",
+    "pull_request_review",
+    "pull_request_review_comment",
+    "push",
+    "release",
+    "repository",
+    "status",
+    "team_add",
+    "watch",
+  ]
+
+  configuration {
+    url          = "https://app.datadoghq.com/intake/webhook/github?api_key=${var.datadog_api_key_for_github}"
+    content_type = "json"
+    insecure_ssl = false
+  }
+}
---
+++ infrastructure/terraform/github/team.tf
@@ -0,0 +1,85 @@
+variable "admins" {
+  type = "list"
+
+  default = [
+    "geowa4",
+    "dantswain",
+  ]
+}
+
+variable "members" {
+  type = "list"
+
+  default = [
+    "bdesham",
+    "brandonramirez",
+    "chorn",
+    "jfine",
+    "RyPeck",
+    "Scotchester",
+    "sfacci",
+  ]
+}
+
+variable "webmasters" {
+  type = "list"
+
+  default = [
+    "Scotchester",
+    "sfacci",
+  ]
+}
+
+variable "legacy_web_repos" {
+  type = "list"
+
+  default = [
+    "homepage",
+    "585-software.github.io",
+  ]
+}
+
+resource "github_membership" "admin_membership" {
+  count    = "${length(var.admins)}"
+  username = "${element(var.admins, count.index)}"
+  role     = "admin"
+}
+
+resource "github_membership" "regular_membership" {
+  count    = "${length(var.members)}"
+  username = "${element(var.members, count.index)}"
+  role     = "member"
+}
+
+resource "github_team" "webmasters" {
+  name        = "Webmasters"
+  description = "Authors of web applications."
+  privacy     = "closed"
+}
+
+resource "github_team_membership" "webmaster_maintainer_membership" {
+  count    = "${length(var.admins)}"
+  team_id  = "${github_team.webmasters.id}"
+  username = "${element(var.admins, count.index)}"
+  role     = "maintainer"
+}
+
+resource "github_team_membership" "webmaster_membership" {
+  count    = "${length(var.webmasters)}"
+  team_id  = "${github_team.webmasters.id}"
+  username = "${element(var.webmasters, count.index)}"
+  role     = "member"
+}
+
+resource "github_team_repository" "webmasters_app_repo" {
+  team_id    = "${github_team.webmasters.id}"
+  repository = "${github_repository.app_repo.name}"
+  permission = "push"
+}
+
+resource "github_team_repository" "webmasters_legacy_repo" {
+  count      = "${length(var.legacy_web_repos)}"
+  team_id    = "${github_team.webmasters.id}"
+  repository = "${element(var.legacy_web_repos, count.index)}"
+  permission = "push"
+}
