93a794e03f7db6504f66ee6764803c046138767c
George Adams IV (geowa4) <oss@geowa4.software>
2017-11-01 00:00:07 -0400

    Make /social and tweak social links in nav.

--- app/lib/rocdev_web/router.ex
+++ app/lib/rocdev_web/router.ex
@@ -14,16 +14,22 @@ defmodule RocdevWeb.Router do
   end
 
   scope "/", RocdevWeb do
-    pipe_through :browser # Use the default browser stack
+    pipe_through :browser
 
     get "/", PageController, :index
     get "/code-of-conduct", PageController, :code_of_conduct
-    get "/slack", PageController, :slack
-    get "/meetup", PageController, :meetup
-    get "/github", PageController, :github
     get "/unsubscribe", SubscriptionController, :unsubscribe
 
     delete "/subscriptions", SubscriptionController, :delete
+  end
+
+  scope "/social", RocdevWeb do
+    pipe_through :browser
+
+    get "/", SocialController, :index
+    get "/slack", SocialController, :slack
+    get "/meetup", SocialController, :meetup
+    get "/github", SocialController, :github
   end
 
   # Other scopes may use custom stacks.
