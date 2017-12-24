db78bcfb98723603525a82eb149a0314de319599
George Adams IV (geowa4) <oss@geowa4.software>
2017-12-23 22:43:56 -0500

    remove unused secret config file reference

--- app/config/prod.exs
+++ app/config/prod.exs
@@ -15,11 +15,20 @@ use Mix.Config
 # which you typically run after static files are built.
 config :rocdev, RocdevWeb.Endpoint,
   load_from_system_env: true,
-  url: [host: "example.com", port: 80],
+  url: [host: "alpha.rocdev.org", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
 config :logger, level: :info
+
+# Configure your database
+config :rocdev, Rocdev.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  username: "rocdev",
+  password: "",
+  database: "rocdev",
+  hostname: "localhost",
+  pool_size: 10
 
 # ## SSL Support
 #
@@ -61,4 +70,4 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
-import_config "prod.secret.exs"
+# import_config "prod.secret.exs"
