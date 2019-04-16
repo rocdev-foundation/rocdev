62e6fb7604749517094e264bbc622cd80bc2255b
Tim <tim@skypanther.com>
2019-04-15 20:25:36 -0400

    Add slack signup form

--- web/about/views.py
+++ web/about/views.py
@@ -1,13 +1,29 @@
+import events
 import logging
 from django.shortcuts import render
-import events
+from .forms import ChatSignupForm
 
 logger = logging.getLogger('rocdev')
 
 
 def index(request):
+    context = {
+        'next_event': events.get_next(),
+        'has_registered': False,
+        'chat_form': None,
+        'num_members': 100,
+        'num_active': 50,
+    }
+    if request.method == 'POST':
+        chat_form = ChatSignupForm(request.POST)
+        if chat_form.is_valid():
+            # do the actual signup / invite sending here
+            context.update({'has_registered': True})
+    else:
+        chat_form = ChatSignupForm()
+        context.update({'chat_form': chat_form})
     return render(
-        request, 'about/home.html', context={'next_event': events.get_next()})
+        request, 'about/home.html', context=context)
 
 
 def code_of_conduct(request):
