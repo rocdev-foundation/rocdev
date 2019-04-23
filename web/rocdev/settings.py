c095a9e2c911d9a40301dd833c04a64394097186
George Adams IV (geowa4) <oss@geowa4.software>
2019-04-22 23:10:32 -0400

    stage 1 of removing bootstrap

--- web/rocdev/settings.py
+++ web/rocdev/settings.py
@@ -140,7 +140,5 @@ USE_L10N = True
 
 USE_TZ = True
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.1/howto/static-files/
-
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
